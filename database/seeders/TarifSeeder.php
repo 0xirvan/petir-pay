26b5b3b7d7e67901a8d67f46eb50882416a11743
irvan pramana <darknet1255@gmail.com>
2025-07-10 13:22:54 +0700

    feat: add TarifSeeder to populate Tarif model with initial data

---
+++ database/seeders/TarifSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TarifSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Tarif::create([
+            'daya' => '450 VA',
+            'tarif_per_kwh' => 415.00,
+            'keterangan' => 'Tarif untuk pelanggan dengan daya 450 VA.',
+        ]);
+
+        \App\Models\Tarif::create([
+            'daya' => '900 VA',
+            'tarif_per_kwh' => 605.00,
+            'keterangan' => 'Tarif untuk pelanggan dengan daya 900 VA.',
+        ]);
+
+        \App\Models\Tarif::create([
+            'daya' => '1300 VA',
+            'tarif_per_kwh' => 1_467.28,
+            'keterangan' => 'Tarif untuk pelanggan dengan daya 1300 VA.',
+        ]);
+
+        \App\Models\Tarif::create([
+            'daya' => '2200 VA',
+            'tarif_per_kwh' => 1_467.28,
+            'keterangan' => 'Tarif untuk pelanggan dengan daya 2200 VA.',
+        ]);
+    }
+}
