b64453e9f8a2b9f5232ba984cb71553addf35c74
irvan pramana <darknet1255@gmail.com>
2025-07-10 13:14:59 +0700

    feat: create Pelanggan and Tarif models with migrations for database structure

---
+++ app/Models/Pelanggan.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Pelanggan extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'pelanggan';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'nama',
+        'email',
+        'password',
+        'alamat',
+        'id_tarif',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
