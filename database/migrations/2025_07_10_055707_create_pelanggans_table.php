be75e61cf5addeab8bb13c7759f82bea857174d6
0xirvan <darknet1255@gmail.com>
2025-07-15 00:13:27 +0700

    fix: change 'nomor_meter' column type from text to string for better data handling

--- database/migrations/2025_07_10_055707_create_pelanggans_table.php
+++ database/migrations/2025_07_10_055707_create_pelanggans_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->string('password');
             $table->text('alamat');
-            $table->text('nomor_meter')->unique();
+            $table->string('nomor_meter')->unique();
             $table->foreignId('id_tarif')->nullable()->constrained('tarif')->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
